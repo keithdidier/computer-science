ef8c2591bd2826d03c5817543d7f52fc6671f9a8
keithdidier <keith.b.didier@gmail.com>
2018-01-25 11:57:13 -0500

    01/25/2018

--- practice /solutions.js
+++ practice /solutions.js
@@ -168,3 +168,139 @@ function fizzBuzz(n) {
         }
     }
 }
+
+
+
+//****Problem 6****
+
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+
+// Solution #1
+function chunk(array, size) {
+    const chunked = [];
+
+    for(let element of array) {
+        const last = chunked[chunked.length -1];
+
+        if(!last || last.length === size) {
+            chunked.push([element]);
+        } else {
+            last.push(element);
+        }
+    }
+    
+    return chunked;
+}
+
+
+// Solution #2
+function chunk(array, size) {
+    const chunked = [];
+    let index = 0;
+
+    while(index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
+    }
+
+    return chunked;
+}
+
+
+
+//****Problem 7****
+
+// --- Directions
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
+
+// Solution #1
+function anagrams(stringA, stringB) {
+    const aCharMap = buildCharMap(stringA);
+    const bCharMap = buildCharMap(stringB);
+    
+    if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+        return false;
+    }
+
+    for(let char in aCharMap) {
+        if(aCharMap[char] !== bCharMap[char]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function buildCharMap(str) {
+    const charMap = {};
+
+    for(let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    return charMap;
+}
+
+// Solution #2
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+    return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+}
+
+
+
+//****Problem 8****
+
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+// Solution #1
+function capitalize(str) {
+    const words = [];
+
+    for(let word of str.split(" ")) {
+        words.push(word[0].toUpperCase() + word.slice(1));
+    }
+
+   return words.join(" ");
+}
+
+// Solution #2
+function capitalize(str) {
+    let result = str[0].toUpperCase();
+
+    for(let i = 1; i < str.length; i++) {
+        if(str[i - 1] === " ") {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
+        }
+    }
+
+    return result;
+}
